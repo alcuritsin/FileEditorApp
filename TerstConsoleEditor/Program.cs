55497903449c497fd26a51c05d3fe18a7861461a
Starinin Andrey <starininandrey@gmail.com>
2021-08-11 17:21:06 +0300

    Добавление тестового проекта

---
+++ TerstConsoleEditor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FileEditorLib;
+
+namespace TerstConsoleEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Stari\Desktop\Новый текстовый документ (3).txt";
+
+            var file = new TxtFileWorking();
+            var document = file.Open(path);
+            
+            ShowTxt(document);
+        }
+
+        static void ShowTxt(Document document)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(document.Content);
+            Console.ResetColor();
+        }
+    }
+}
