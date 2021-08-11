6922dffc86897621e415a503d4b2e5fdf0903dd3
Starinin Andrey <starininandrey@gmail.com>
2021-08-11 18:35:11 +0300

    Добавление библиотеки для работы с консолью.

---
+++ ConsoleLib/ConsoleEx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FileEditorLib;
+
+namespace ConsoleLib
+{
+    public static class ConsoleEx
+    {
+        public static void Show(Document document)
+        {
+            var type = document.GetType().FullName;
+            switch (type)
+            {
+                case "FileEditorLib.MdDocument":
+                    ShowMd(document);
+                    break;
+                case "FileEditorLib.TxtDocument":
+                    ShowTxt(document);
+                    break;
+            }
+        }
+
+        private static void ShowTxt(Document document)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(document.Content);
+            Console.ResetColor();
+        }
+
+        private static void ShowMd(Document document)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(document.Content);
+            Console.ResetColor();
+        }
+    }
+}
